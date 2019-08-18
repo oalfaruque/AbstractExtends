331c5f8bf0023023c68db2315a502be864f0537a
oalfaruque <oalfaruque@gmail.com>
2019-08-18 17:13:40 -0400

    Abstract and Extand

---
+++ IdeaProjects/AbstractExtends/src/AbstractExtends/landPhone.java
@@ -0,0 +1,11 @@
+package AbstractExtends;
+
+public abstract class landPhone {
+    public void size(){
+        System.out.println("Phone is being used ");}
+    public void call(){
+        System.out.println("We can make a call with the phone");
+        public abstract void text(){
+            System.out.println("We can send sms");}        }
+    }
+
---
+++ IdeaProjects/AbstractExtends/src/AbstractExtends/phones.java
@@ -0,0 +1,7 @@
+package AbstractExtends;
+
+public interface phones {
+    public void size();
+    public void call();
+    public void text();
+}
